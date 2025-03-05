846f346016f2717d020217b7fa1b6d1e44e6a381
nabil sikati <147974823+nsk755@users.noreply.github.com>
2025-03-05 12:00:28 +0100

    Update RSA-d-Calc.py

--- RSA-d-Calc.py
+++ RSA-d-Calc.py
@@ -1,8 +1,8 @@
-e = input("e = ")
-phi = input("phi = ")
+e = int(input("was ist der wert von e: "))
+phi = int(input("was ist der wert von phi: "))
 for d in range(1000):
     result = (e * d) % phi
     if result == 1:
         print(d)
         break
-    
+
